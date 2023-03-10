476e717309e5b7e84dc4aefcd211e9eb58cc53c9
berketurker <45368526+berkosko5000@users.noreply.github.com>
2023-03-10 17:31:33 +0300

    Kuduz

--- Assets/Scripts/Box/BoxCreator.cs
+++ Assets/Scripts/Box/BoxCreator.cs
@@ -9,17 +9,29 @@ public class BoxCreator : MonoBehaviour
 
     void Start()
     {
-        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
-        Vector2 gridSize = new Vector2(columns * cellSize, rows * cellSize);
-        Vector2 gridOffset = (screenSize - gridSize) / 2f;
+        Camera mainCamera = Camera.main;
+        if (mainCamera == null)
+        {
+            Debug.LogError("No main camera found in the scene.");
+            return;
+        }
 
+        Vector2 screenSize = new Vector2(mainCamera.pixelWidth, mainCamera.pixelHeight);
+        float aspectRatio = mainCamera.aspect;
+        float cameraHeight = mainCamera.orthographicSize * 2f;
+        float cameraWidth = cameraHeight * aspectRatio;
+
+        Vector2 gridSize = new Vector2(columns * cellSize, rows * cellSize);
+        Vector2 gridScale = new Vector2(cameraWidth*0.1f / gridSize.x, cameraHeight * 0.6f / gridSize.y);
+        Vector2 gridOffset = new Vector2(mainCamera.transform.position.x - cameraWidth / 2f + cameraWidth * 0.05f, mainCamera.transform.position.y - cameraHeight / 2f + cameraHeight * 0.1f
+);
         for (int y = 0; y < rows; y++)
         {
             for (int x = 0; x < columns; x++)
             {
                 Vector2 spawnPosition = new Vector2(
-                    x * cellSize + cellSize / 2f + gridOffset.x,
-                    y * cellSize + cellSize / 2f + gridOffset.y
+                    x * cellSize * gridScale.x + cellSize * gridScale.x / 2f + gridOffset.x,
+                    y * cellSize * gridScale.y + cellSize * gridScale.y / 2f + gridOffset.y
                 );
 
                 GameObject gridObject = Instantiate(gridPrefab, spawnPosition, Quaternion.identity);
